020d4091e86d4703db34cfcc60b2c1aa2cc6ffb7
fitzies <olifitz1610@gmail.com>
2024-06-02 16:05:16 +0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+const Page = ({ params }: { params: { slug: string } }) => {
+  return <div className="w-screen h-screen flex items-center justify-center">Next js with Shadcn</div>
+}
+
+export default Page
